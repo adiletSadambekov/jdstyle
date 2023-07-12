7f3fd625865f707776c3ab65b6212279cf22743e
Adilet <adiletsadambekov001@gmail.com>
2023-07-12 17:42:40 +0600

    fix static

--- jd_tseh/settings.py
+++ jd_tseh/settings.py
@@ -134,9 +134,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/api/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/api/images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
